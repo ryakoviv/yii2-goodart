bec7b941f6b38949148c18690f0f78cedeb17821
ryakoviv <ryakoviv@gmail.com>
2016-09-24 11:34:40 +0300

    Added style to portfolio section and added price functionality

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -13,24 +13,23 @@ $this->title = 'My Yii Application';
                 і красивіше на стіні виглядає мальовничий портрет!
             </p>
             <div class="row top_box">
-                <div class="col-md-6 contentleft_grid">
+                <div class="col-lg-6 col-md-6 col-sm-6 contentleft_grid">
                     <h3 class="m_2"><i class="pen"></i>Ми допоможемо вам з вибором</h3>
                     <p class="m_3">Ми малюємо портрети фарбами або звичайним олівцем, враховуючи всі ваші побажання. Ви можете замовити портрет класичний,сімейний портрет, портрет в стилі поп-арт.</p>
 <!--                    <a href="#" class="content_btn">More Info</a>-->
                 </div>
-                <div class="col-md-6">
+                <div class="col-lg-6 col-md-6 col-sm-6">
                     <h3 class="m_2"><i class="tick"></i>Зробити замовлення дуже просто</h3>
                     <p class="m_3">Усе, що Вам потрібно зробити, щоб замовити портрет - це принести або переслати якісні фотографії або інші зображення, які Ви б хотіли перенести на полотно.</p>
-<!--                    <a href="#" class="content_btn">More Info</a>-->
                 </div>
             </div>
             <div class="row top_box">
-                <div class="col-md-6 contentleft_grid">
+                <div class="col-lg-6 col-md-6 col-sm-6 contentleft_grid">
                     <h3 class="m_2"><i class="pen"></i>Вам не прийдеться довго чекати</h3>
                     <p class="m_3">Час виконання залежить від черги замовлень, може бути від 2х до 10 днів. Тож краще оформити замовлення раніше!</p>
                     <!--                    <a href="#" class="content_btn">More Info</a>-->
                 </div>
-                <div class="col-md-6">
+                <div class="col-lg-6 col-md-6 col-sm-6">
                     <h3 class="m_2"><i class="tick"></i>Для нас немає значення місце і час</h3>
                     <p class="m_3">Замовлення приймаються будь-коли та з різних куточків України.</p>
                     <!--                    <a href="#" class="content_btn">More Info</a>-->
@@ -48,14 +47,14 @@ $this->title = 'My Yii Application';
 
             <div class="row service_top">
                 <div class="col-md-4 service_grid">
-                    <i class="icon1"></i>
-                    <h3><a href="#">Чорно-білі портрети олівцем</a></h3>
-                    <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros</p>
-                </div>
-                <div class="col-md-4 service_grid">
                     <i class="icon2"></i>
                     <h3><a href="#">Портрети в стилі поп-арт</a></h3>
                     <p>Nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. </p>
+                </div>
+                <div class="col-md-4 service_grid">
+                    <i class="icon1"></i>
+                    <h3><a href="#">Чорно-білі портрети олівцем</a></h3>
+                    <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros</p>
                 </div>
                 <div class="col-md-4 service_grid">
                     <i class="icon3"></i>
@@ -76,10 +75,17 @@ $this->title = 'My Yii Application';
                 <div class="wrap">
                     <div class="porfolio-section">
                         <div class="porfolio-section-title">
-                                <h3 class="m_5" style="border-top: 1px solid #4a576e;border-bottom: 1px solid #4a576e;">Портрети поп-арт</h3>
+                                <h3>Портрети поп-арт</h3>
                         </div>
                         <div class="porfolio-section-content">
                             <div class="fotorama" data-max-height="100%" data-max-width="100%" data-nav="thumbs" data-allowfullscreen="native">
+                                <img src="images/pop1.jpg">
+                                <img src="images/pop2.jpg">
+                                <img src="images/pop3.jpg">
+                                <img src="images/pop4.jpg">
+                                <img src="images/pop5.jpg">
+                                <img src="images/pop6.jpg">
+                                <img src="images/pop7.jpg">
                                 <img src="images/pop1.jpg">
                                 <img src="images/pop2.jpg">
                                 <img src="images/pop3.jpg">
@@ -93,17 +99,17 @@ $this->title = 'My Yii Application';
 
                     <div class="porfolio-section">
                         <div class="porfolio-section-title">
-                            <h3 class="m_5" style="border-top: 1px solid #4a576e;border-bottom: 1px solid #4a576e;">Портрети олівцем</h3>
+                            <h3>Портрети олівцем</h3>
                         </div>
                         <div class="porfolio-section-content">
                             <div class="fotorama" data-max-height="100%" data-max-width="100%" data-nav="thumbs" data-allowfullscreen="native">
-                                <img src="images/pop1.jpg">
-                                <img src="images/pop2.jpg">
-                                <img src="images/pop3.jpg">
-                                <img src="images/pop4.jpg">
-                                <img src="images/pop5.jpg">
-                                <img src="images/pop6.jpg">
-                                <img src="images/pop7.jpg">
+                                <img src="images/pen1.jpg">
+                                <img src="images/pen2.jpg">
+                                <img src="images/pen3.jpg">
+                                <img src="images/pen4.jpg">
+                                <img src="images/pen5.jpg">
+                                <img src="images/pen6.jpg">
+                                <img src="images/pen7.jpg">
                             </div>
                         </div>
                     </div>
@@ -112,6 +118,63 @@ $this->title = 'My Yii Application';
         </div>
     </div>
     <!--portfolio end here-->
+
+    <div class="content_middle" id="price">
+        <div class='container'>
+            <h2 class="head">Ціни</h2>
+            <p class="service_desc">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam dignissimos eaque et expedita fuga natus perferendis quae quam quos voluptates. Accusamus accusantium aliquid animi corporis eligendi laboriosam magnam, numquam sit.
+            </p>
+            <div class="row service_top">
+                <div class="col-md-offset-1 col-md-4  col-sm-offset-1 col-sm-4  col-xs-offset-1 col-xs-5 price_grid" id="price-pop-art">
+                    <h3>портрет поп-арт</h3>
+                    <div class="form-group">
+                        <select class="form-control picture-size">
+                            <option>Виберіть розмір</option>
+                            <option>Формат А3 (15х21)</option>
+                            <option>Формат А2 (21х30)</option>
+                            <option>Формат А1 (30х40)</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <select class="form-control picture-people-number">
+                            <option>Виберіть к-сть людей</option>
+                            <option>1 людина на портреті</option>
+                            <option>2 людей на портреті</option>
+                            <option>3 людей на портреті</option>
+                        </select>
+                    </div>
+                    <p class="error-block hidden">На вибраному Вами розмірі портрета не може бути зображено така кількість людей.</p>
+                    <p class="error-block hidden">Виберіть будь ласка інші параметри</p>
+                    <p class="price-calc-btn content_btn">Визначити ціну</p>
+                </div>
+
+                <div class="col-md-offset-1 col-md-4  col-sm-offset-1 col-sm-4  col-xs-offset-1 col-xs-5 price_grid" id="price-pencil">
+                    <h3>портрет олівцем</h3>
+                    <div class="form-group">
+                        <select class="form-control picture-size">
+                            <option>Виберіть розмір</option>
+                            <option>Формат А3 (15х21)</option>
+                            <option>Формат А2 (21х30)</option>
+                            <option>Формат А1 (30х40)</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <select class="form-control picture-people-number">
+                            <option>Виберіть к-сть людей</option>
+                            <option>1 людина на портреті</option>
+                            <option>2 людей на портреті</option>
+                            <option>3 людей на портреті</option>
+                        </select>
+                    </div>
+                    <p class="error-block hidden">На вибраному Вами розмірі портрета не може бути зображено така кількість людей.</p>
+                    <p class="error-block hidden">Виберіть будь ласка інші параметри</p>
+                    <p class="price-calc-btn content_btn">Визначити ціну</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
 
     <div class="contact" id="contact">
         <div class="container">
--- frontend/web/js/custom.js
+++ frontend/web/js/custom.js
@@ -1,9 +1,52 @@
 /**
  * Created by ryakoviv on 11.09.2016.
  */
-// Can also be used with $(document).ready()
-// $(window).load(function() {
-//     $('#portfolio .wrap').flexslider({
-//         animation: "slide"
-//     });
-// });
+$(document).ready(function() {
+    $('#price .price_grid .content_btn').on("click", function (e) {
+        var getHtmlFromSelect = function (arr) {
+            for (var i=0; i<arr.length; i++){
+                arr[i] = $(arr[i]).text();
+            }
+            return arr;
+        };
+
+        e.preventDefault();
+        var parent = $(e.target).parent();
+        var arrSize = getHtmlFromSelect($(parent).find(".picture-size option"));
+        var selectedSize = $(parent).find(".picture-size").find(":selected").text();
+        var selectedSizeNumber = $.inArray(selectedSize, arrSize);
+        var arrPeople = getHtmlFromSelect($(parent).find(".picture-people-number option"));
+        var selectedPeople = $(parent).find(".picture-people-number").find(":selected").text();
+        var selectedPeopleNumber = $.inArray(selectedPeople, arrPeople);
+        var error = $(parent).find(".error-block");
+        var prices = [];
+        switch ($(parent).attr("id")){
+            case "price-pencil":
+                prices[0] = [0, 0, 0, 0];
+                prices[1] = [0, 30, 50, 75];
+                prices[2] = [0, 0, 75, 130];
+                prices[3] = [0, 0, 0, 170];
+                break;
+            case "price-pop-art":
+                prices[0] = [0, 0, 0, 0];
+                prices[1] = [0, 330, 450, 600];
+                prices[2] = [0, 350, 500, 650];
+                prices[3] = [0, 0, 550, 700];
+                break;
+        }
+
+        var endPrice = prices[selectedPeopleNumber][selectedSizeNumber];
+        if (endPrice){
+            $(error).addClass("hidden");
+            $(this).addClass("price-value").removeClass("content_btn").html(endPrice+"грн");
+        }
+        else if(!endPrice && selectedPeopleNumber && selectedSizeNumber) {
+            $(error).removeClass("hidden");
+        }
+    });
+
+    $('#price .price_grid select').on("click", function (e) {
+        $(e.target).parents(".price_grid").find(".error-block").addClass("hidden");
+        $(e.target).parents(".price_grid").find(".price-calc-btn").addClass("content_btn").removeClass("price-value").html("Визначити ціну");
+    });
+});
